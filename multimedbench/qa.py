4ad61983605c0d6a23510305fce4fdba862c45d1
Corentin Royer <corentin.royer@uzh.ch>
2023-12-21 18:31:10 +0100

    Improved QA

--- multimedbench/qa.py
+++ multimedbench/qa.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import math
 from multimedbench.utils import remove_punctuation
 import random
+
 # from nltk.corpus import stopwords
 # import nltk
 from abc import abstractmethod
@@ -114,7 +115,7 @@ class MedQA(QA):
         formattedQuestion += (
             "Options:\n" + "\n".join([f'{option["key"]}: {option["value"]}.' for option in options]) + "\n"
         )
-        formattedQuestion += "What is the correct answer?\n"
+        formattedQuestion += "What is the correct answer?"
         formattedAnswer = "The answer is " + (answer if prompt else "") + "."
 
         question = [{"role": "user", "content": formattedQuestion}]
@@ -133,17 +134,6 @@ class MedQA(QA):
         pred = self.cleanStr(pred)
         if len(pred) == 0:
             return False
-
-        # optionVocabs = [self.cleanStr(f'{option["key"]}: {option["value"]}').split(" ") for option in sample["options"]]
-        # optionVocabs = [[word for word in option if word not in STOPWORDS] for option in optionVocabs]
-
-        # pred = pred.split(" ")
-        # pred = [word for word in pred if word not in STOPWORDS]
-        # scores = [0 for _ in range(len(optionVocabs))]
-        # for token in pred:
-        #     for idx in range(len(optionVocabs)):
-        #         if token in optionVocabs[idx]:
-        #             scores[idx] += 1
 
         options = [self.cleanStr(f'{option["key"]}: {option["value"]}') for option in sample["options"]]
         # Compute the BLEU score for each option
@@ -187,7 +177,7 @@ class PubMedQA(QA):
         answer = sample["answer"]
 
         formattedQuestion = f"{context}\nQuestion: {question}\n"
-        formattedQuestion += "Answer with yes, no or maybe.\n"
+        formattedQuestion += "Answer with yes, no or maybe."
 
         formattedAnswer = answer[0]
 
@@ -240,8 +230,6 @@ class MedMCQA(QA):
 
         self.prompt = self.getPrompt()
 
-        self.mapToNumber = ["1", "2", "3", "4"]
-
         self.bleuScorer = BLEUScore()
 
     def format_question(self, sample, prompt=False):
@@ -256,7 +244,7 @@ class MedMCQA(QA):
 
         formattedQuestion = f"{question}\n"
         formattedQuestion += "Options:\n" + "\n".join(options) + "\n"
-        formattedQuestion += "What is the correct answer?\n"
+        formattedQuestion += "What is the correct answer?"
 
         formattedAnswer = f"The answer is {options[answer]}."
 
@@ -266,36 +254,30 @@ class MedMCQA(QA):
         return (question, [])
 
     def getCorrectAnswer(self, sample, fullText=False):
-        number = self.mapToNumber[sample["cop"]]
-        text = sample["co" + str(sample["cop"])]
+        number = sample["cop"]
         if fullText:
-            return f"{number}: {text}"
+            return self._getOptions(sample)[number]
         else:
-            return number
+            return str(number + 1)
+
+    def _getOptions(self, sample):
+        return [
+            f"1: {sample['opa']}.",
+            f"2: {sample['opb']}.",
+            f"3: {sample['opc']}.",
+            f"4: {sample['opd']}.",
+        ]
 
     def isValid(self, pred: str, sample):
         pred = self.cleanStr(pred)
         if len(pred) == 0:
             return False
 
-        # optionVocabs = [f"1: {sample['opa']}.", f"2: {sample['opb']}.", f"3: {sample['opc']}.", f"4: {sample['opd']}."]
-        # optionVocabs = [self.cleanStr(option).split(" ") for option in optionVocabs]
-        # optionVocabs = [[word for word in option if word not in STOPWORDS] for option in optionVocabs]
-
-        # pred = pred.split(" ")
-        # pred = [word for word in pred if word not in STOPWORDS]
-
-        # scores = [0 for _ in range(len(optionVocabs))]
-        # for token in pred:
-        #     for idx in range(len(optionVocabs)):
-        #         if token in optionVocabs[idx]:
-        #             scores[idx] += 1
-
         options = [f"1: {sample['opa']}.", f"2: {sample['opb']}.", f"3: {sample['opc']}.", f"4: {sample['opd']}."]
         # Compute the BLEU score for each option
         scores = [self.bleuScorer([self.cleanStr(option)], [pred]) for option in options]
 
-        pred = self.mapToNumber[scores.index(max(scores))]
+        pred = scores.index(max(scores)) + 1  # +1 because the options are 1, 2, 3, 4 and not 0, 1, 2, 3
 
         gold = self.getCorrectAnswer(sample)[0]
         return pred == gold
