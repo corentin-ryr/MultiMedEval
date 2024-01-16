c7ce929f6bd74dce02a579f7b49ebd63d440a01e
Corentin Royer <corentin.35035@gmail.com>
2024-01-16 18:16:51 +0100

    Changed requirements and modified setup

--- testImageClassification.py
+++ testImageClassification.py
@@ -5,7 +5,4 @@ def batcher(prompts):
 
 engine = MMB(params=Params(batch_size=64), batcher=batcher, generateVisualization=True)
 
-raise Exception
-engine.eval(["MedQA","PubMedQA","MedMCQA","MIMIC-CXR-ReportGeneration",
-             "VQA-RAD","Path-VQA","SLAKE","MIMIC-CXR-ImageClassification",
-             "VinDr-Mammo","Pad-UFES-20","CBIS-DDSM","MIMIC-III"])
+engine.eval(["MIMIC-CXR-ImageClassification"])
