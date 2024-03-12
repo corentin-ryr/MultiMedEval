b923ee0101f47c84c970b138ec0b51eedc6ed1e2
Corentin Royer <corentin.35035@gmail.com>
2024-03-12 16:15:28 +0100

    Included patient info in pad ufes 20

--- multimedeval/imageClassification.py
+++ multimedeval/imageClassification.py
@@ -298,13 +298,13 @@ class Pad_UFES_20(ImageClassification):
             "has sewage system": sample["has_sewage_system"],
         }
         # Create a sentence out of the patient information don't include Nones
-        # patientInfo = ", ".join([f"{key} {value}" for key, value in patientInfo.items() if value is not None])
+        patientInfo = "Patient history: " + ", ".join([f"{key} {value}" for key, value in patientInfo.items() if value is not None])
         options = "Options:\n" + "\n".join([self.mapAcronymToName[option] for option in self.options])
 
         formattedText = [
             {
                 "role": "user",
-                "content": f"<img> {options} What is the most likely diagnosis among the following propositions?",
+                "content": f"<img> {patientInfo} Which of the following is the most likely diagnosis of the patient's skin lesion? {options}",
             }
         ]
 
